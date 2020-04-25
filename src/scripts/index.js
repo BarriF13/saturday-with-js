7fdbaf82a34ab164a2adfcd208dad32658313306
Barri <info@bdesignstudio.co.uk>
2020-04-25 10:46:48 +0100

    Rest parameters

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,4 +1,6 @@
-import '../styles/index.scss';
+//---Rest parameter in new JS --- it is a parameter that we can pass multiple arguments in it and it is the last argument in parameter and has 3 dotd at the beginning(...restparameter)
 
-console.log('webpack starterkit');
-console.log('hello');
+function sendCars(day, ...allCarIds) {
+  allCarIds.forEach( id => console.log(id) );
+}
+sendCars('Monday', 1,2,3);
