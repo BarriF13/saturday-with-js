51612ecb6ee145079b18651afda0eb2799aa729e
Barri <info@bdesignstudio.co.uk>
2020-04-25 11:19:57 +0100

    Destructuring Object

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,18 +1,29 @@
 //---Rest parameter in new JS --- it is a parameter that we can pass multiple arguments in it and it is the last argument in parameter and has 3 dotd at the beginning(...restparameter)
 
-function sendCars(day, ...allCarIds) {
-  allCarIds.forEach( id => console.log(id) );
-}
-sendCars('Monday', 1,2,3);
+// function sendCars(day, ...allCarIds) {
+//   allCarIds.forEach( id => console.log(id) );
+// }
+// sendCars('Monday', 1,2,3);
 
 //Destructuring Arrays
-let carIds = [1,2,5];
-let [car1, car2, car3] = carIds;// if we don't put car3 the number 5 wont's be assign 
+// let carIds = [1,2,5];
+// let [car1, car2, car3] = carIds;// if we don't put car3 the number 5 wont's be assign 
 
-console.log(car1, car2, car3);
+// console.log(car1, car2, car3);
 //Another version
 
-let carIDS = [1 ,4, 5];
-let carr1 , remainingCars;
-[carr1, ...remainingCars]= carIDS;
-console.log(car1 , remainingCars);
+// let carIDS = [1 ,4, 5];
+// let carr1 , remainingCars;
+// [carr1, ...remainingCars]= carIDS;
+// console.log(car1 , remainingCars);
+
+//--Destructuring Objects
+// let car = { id:500, style: 'convertible' };
+// let {id, style} = car;
+// console.log(id, style);
+
+// or like this 
+let car = { id:500, style: 'convertible' };
+let id , style;
+( {id, style} = car);
+console.log(id, style);
